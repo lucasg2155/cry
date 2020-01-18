291cc79092a74fdf24ad0beeee6c193861d33fa0
lucasg2155 <ggcon.gabriel@gmail.com>
2020-01-18 17:25:40 -0300

    cry 8 commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -101,7 +101,7 @@ const Footer = class extends React.Component {
                     src={vimeo}
                     alt="Vimeo"
                     style={{ width: '1em', height: '1em' }}
-                  />*/} <h3>&copi;</h3>2020 por ggcon
+                  />*/}© 2020 por ggcon
                 </a>
               </div>
             </div>
