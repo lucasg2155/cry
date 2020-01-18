33b12e6b87c8851ddaa1e9cf0eefc20bfa6043fe
lucasg2155 <ggcon.gabriel@gmail.com>
2020-01-18 16:14:12 -0300

    cry 4 commit

--- src/pages/contact/file-upload.js
+++ src/pages/contact/file-upload.js
@@ -46,7 +46,7 @@ export default class Contact extends React.Component {
         <section className="section">
           <div className="container">
             <div className="content">
-              <h1>File Upload</h1>
+              <h1>Subir archivo</h1>
               <form
                 name="file-upload"
                 method="post"
@@ -65,7 +65,7 @@ export default class Contact extends React.Component {
                 </div>
                 <div className="field">
                   <label className="label" htmlFor={'name'}>
-                    Your name
+                    Nombre
                   </label>
                   <div className="control">
                     <input
@@ -88,14 +88,14 @@ export default class Contact extends React.Component {
                         onChange={this.handleAttachment}
                       />
                       <span className="file-cta">
-                        <span className="file-label">Choose a file…</span>
+                        <span className="file-label">Escoger archivo…</span>
                       </span>
                     </label>
                   </div>
                 </div>
                 <div className="field">
                   <button className="button is-link" type="submit">
-                    Send
+                    Enviar
                   </button>
                 </div>
               </form>
